2fd96e3f7d0421468d29a058b87d21333e9940f2
Jacob Bridges <me@jacobbridges.dev>
2025-01-30 01:12:45 -0600

    fix Path.mkdir

--- src/api/core/db.py
+++ src/api/core/db.py
@@ -7,5 +7,5 @@ def get_conn():
     """
     Get a connection to the SQLite database.
     """
-    data_dir = Path("data").mkdir(exists_ok=True)
+    data_dir = Path("data").mkdir(exist_ok=True)
     return aiosqlite.connect("data/sqlite.db")
